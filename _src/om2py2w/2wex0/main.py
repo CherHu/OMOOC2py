e49100a3c49eae7291bda1cfa30eadc4d58a264f
CherHu <cherhoo@126.com>
2015-11-16 14:37:37 +0800

    2wex0

---
+++ _src/om2py2w/2wex0/main.py
@@ -0,0 +1,46 @@
+# coding = utf-8
+#!/user/bin/env python
+
+import Tkinter as tk
+import os
+import sys
+from ScrolledText import ScrolledText
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+root = tk.Tk()
+root.title("CHER'S DIARY")
+
+def saveClick(self):
+	with open(os.getcwd()+'mydiary','w+') as fb:
+	   fb.write(text.get(0.0,'end)')) 
+
+	
+def write():
+    append_text(var.get())
+    text_output.insert(0.0, text.get())
+    var.set('')
+	
+master = tk.Frame(root)
+master.pack()
+
+text=tk.Text(master)
+
+text=tk.StringVar(value="Hi, wanna talk? Here we go.")
+text_insert = tk.Entry(master, textvariable = text)
+text_insert.pack()
+
+master.bind('<Return>',write)
+
+text_output=ScrolledText(master)
+text_output.pack()
+
+save=tk.Button(master, text="save",fg='magenta',command=saveClick)
+save.bind('<Command-S>',saveClick) # doesn't work yet
+save.pack()
+
+quit=tk.Button(master, text="close",fg='magenta', command=master.quit)
+quit.pack(side='bottom')
+
+root.mainloop()
